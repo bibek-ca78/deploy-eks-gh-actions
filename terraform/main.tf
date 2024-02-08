724ba25d5438fd64f0c564930688e0dc0635512d
bibek <bibek2003@gmail.com>
2024-02-08 18:08:49 +0530

    adding eks deployment terraform resource - corrected AZ

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,13 +28,13 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"  # Specify availability zone
+  aavailability_zone =  "us-east-1a" # Specify availability zone
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.2.0/24"
-  availability_zone       = "us-east-1a"  # Specify availability zone
+  availability_zone       = "us-east-1b"  # Specify availability zone
 }
 
 # Create a security group for the EKS cluster
