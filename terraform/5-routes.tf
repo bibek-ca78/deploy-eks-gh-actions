4706bbff5ff4e63f02624350d596129db28e5a77
bibek <bibek2003@gmail.com>
2024-02-09 15:33:43 +0530

    New codes added replacing the old one - correction 3

--- terraform/5-routes.tf
+++ terraform/5-routes.tf
@@ -1,25 +1,6 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.nat.id
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-      core_network_arn           = ""
-    },
-  ]
-
   tags = {
     Name = "private"
   }
@@ -28,28 +9,21 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
-      nat_gateway_id             = ""
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-      core_network_arn           = ""
-    },
-  ]
-
   tags = {
     Name = "public"
   }
+}
+
+resource "aws_route" "internet_gateway_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "nat_gateway_route" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat.id
 }
 
 resource "aws_route_table_association" "private-us-east-1a" {
